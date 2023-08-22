a1136cacd8d127ddeec38721bdaf67bcaf176308
Trent Deal <trent.deal@gmail.com>
2023-08-22 11:25:15 -0400

    Remove test code from main.py

--- {{ cookiecutter.project_slug }}/main.py
+++ {{ cookiecutter.project_slug }}/main.py
@@ -9,13 +9,3 @@ ENV: str | None = os.environ.get("ENV")
 
 if ENV == "dev":
     start_debugger()
-
-num: int = 0
-rando: int = random.randint(0, 10)
-rando2: int = random.randint(0, 10)
-
-num += rando
-num += rando2
-
-print("What is the number?")
-print(num)
